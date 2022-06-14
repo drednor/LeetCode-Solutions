233df423092ccdaff0155ee4c15abfbde5bf5d8f
drednor <42916508+drednor@users.noreply.github.com>
2022-06-14 20:46:03 +0530

    Time: 154 ms, Memory: 15.3 MB - LeetHub

--- search-for-a-range/search-for-a-range.py
+++ search-for-a-range/search-for-a-range.py
@@ -1,17 +1,42 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        result = [-1,-1]
-        for i in range(len(nums)):
-            if nums[i] == target:
-                result[0] = i
-                break
-        else:
-            return result
-        for i in range(len(nums)-1, -1,-1):
-            if nums[i] == target:
-                result[1] = i
-                break
-        return result
-        # l , r = 0 , len(nums)
-        # while l<=r:
-        #     mid = (l+r)//2
+        left = self.binsearch(nums,target, True)
+        right = self.binsearch(nums, target, False)
+        return [left,right]
+    
+    
+    def binsearch(self,nums, target, leftbias):
+        l , r = 0 , len(nums)-1
+        i = -1
+        while l<=r:
+            mid = (l+r)//2
+            if nums[mid]> target:
+                r = mid -1
+            elif nums[mid]< target:
+                l = mid+1
+            else:
+                i = mid
+                if leftbias:
+                    r = mid-1
+                else:
+                    l = mid+1
+        return i 
+            
+            
+        
+        
+        
+        # ITERATIVE SOLUTION
+        # result = [-1,-1]
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         result[0] = i
+        #         break
+        # else:
+        #     return result
+        # for i in range(len(nums)-1, -1,-1):
+        #     if nums[i] == target:
+        #         result[1] = i
+        #         break
+        # return result
+        
