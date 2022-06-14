90e7662d97195608b0fab644573199da9ae5cc6c
drednor <42916508+drednor@users.noreply.github.com>
2022-06-14 11:18:00 +0530

    Time: 34 ms (83.35%), Space: 13.9 MB (71.66%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -2,23 +2,34 @@ class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
         if s1 == s2:
             return True
-        bad = []
+        x = list(zip(s1,s2))
+        result = 0
+        for i in range(len(x)):
+            if x[i][0] != x[i][1]:
+                result+=1
+        if result != 2:
+            return False
+        elif result == 2 and sorted(s1)==sorted(s2):
+            return True
+        else:
+            return False
+#         bad = []
         
-        for i in range(len(s1)):
-            if s1[i]!=s2[i]:
-                bad.append(i)
+#         for i in range(len(s1)):
+#             if s1[i]!=s2[i]:
+#                 bad.append(i)
         
-        if len(bad) !=2: return False
+#         if len(bad) !=2: return False
         
-        s1 = list(s1)
-        s1[bad[0]], s1[bad[1]] = s1[bad[1]],s1[bad[0]]
+#         s1 = list(s1)
+#         s1[bad[0]], s1[bad[1]] = s1[bad[1]],s1[bad[0]]
         
-        res = ""
+#         res = ""
         
-        for i in s1:
-            res += str(i)
+#         for i in s1:
+#             res += str(i)
         
-        return res == s2
+#         return res == s2
         
                 
         
