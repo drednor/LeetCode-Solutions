8cbd29ca48803052b8aeb15013cda40a9cb931bc
drednor <42916508+drednor@users.noreply.github.com>
2022-06-15 15:58:50 +0530

    Time: 571 ms, Memory: 15.4 MB - LeetHub

--- find-k-closest-elements/find-k-closest-elements.py
+++ find-k-closest-elements/find-k-closest-elements.py
@@ -1,29 +1,37 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        if len(arr) == k:
-            return arr
-        l, r = 0, len(arr)-1
-        val = arr[0]
-        idx = 0
-        while l<=r:
+        l , r = 0 , len(arr)-k
+        while l<r:
             mid = (l+r)//2
-            curdiff, resdiff = abs(arr[mid]-x), abs(val - x)
-            if curdiff<resdiff or curdiff == (resdiff and arr[mid]<val):
-                val , idx = arr[mid], mid
-            if x > arr[mid]:
+            if x-arr[mid] > arr[mid+k] - x:
                 l = mid+1
-            elif x < arr[mid]:
-                r = mid-1
             else:
-                break
-        l = r= idx
-        for i in range(k-1):
-            if l == 0:
-                r += 1
-            elif r == len(arr)-1 or (x - arr[l-1]) <= (arr[r+1] - x):
-                l -= 1
-            else:
-                r+=1
-        return arr[l:r+1]
+                r = mid
+        return arr[l:l+k]
+        # if len(arr) == k:
+        #     return arr
+        # l, r = 0, len(arr)-1
+        # val = arr[0]
+        # idx = 0
+        # while l<=r:
+        #     mid = (l+r)//2
+        #     curdiff, resdiff = abs(arr[mid]-x), abs(val - x)
+        #     if curdiff<resdiff or curdiff == (resdiff and arr[mid]<val):
+        #         val , idx = arr[mid], mid
+        #     if x > arr[mid]:
+        #         l = mid+1
+        #     elif x < arr[mid]:
+        #         r = mid-1
+        #     else:
+        #         break
+        # l = r= idx
+        # for i in range(k-1):
+        #     if l == 0:
+        #         r += 1
+        #     elif r == len(arr)-1 or (x - arr[l-1]) <= (arr[r+1] - x):
+        #         l -= 1
+        #     else:
+        #         r+=1
+        # return arr[l:r+1]
                 
         
