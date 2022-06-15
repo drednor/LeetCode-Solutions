34cbacc171411e71fdd67dc47926ae9afccdd1bb
drednor <42916508+drednor@users.noreply.github.com>
2022-06-15 20:40:51 +0530

    Time: 279 ms, Memory: 15.1 MB - LeetHub

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,7 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        # TWO POINTER APPROACH
         i, j = 0, len(numbers)-1
         while i<j:
             if (numbers[i] + numbers[j]) > target:
