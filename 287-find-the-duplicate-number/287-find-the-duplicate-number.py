f9c812003468d9f0f3eb3718c13a58dab604c511
drednor <42916508+drednor@users.noreply.github.com>
2022-06-16 14:46:02 +0530

    Time: 647 ms (93.13%), Space: 27.9 MB (55.65%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,14 +1,26 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         
+        fast , slow = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            slow2 = nums[slow2]
+            fast = nums[fast]
+            if slow2 == fast:
+                return slow2
         
-        # MAKING A HASHMAP AND ADDING VALUE IF VALUE IS ALREADY SEEN
-        tempmap={}
-        for i in nums:
-            tempmap[i] = tempmap[i]+1 if i in tempmap else 1
-            if tempmap[i] > 1:
-                return i
-        return -1
+        # # MAKING A HASHMAP AND ADDING VALUE IF VALUE IS ALREADY SEEN
+        # tempmap={}
+        # for i in nums:
+        #     tempmap[i] = tempmap[i]+1 if i in tempmap else 1
+        #     if tempmap[i] > 1:
+        #         return i
+        # return -1
         
         #Using Counter 
         # c = Counter(nums)
