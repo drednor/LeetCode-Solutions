24c7955d992a6fd310eee1c2510f2fe42de079bc
drednor <42916508+drednor@users.noreply.github.com>
2022-06-19 00:37:59 +0530

    Time: 529 ms (69.16%), Space: 26 MB (29.81%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,10 +1,18 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        result = {}
+        hashset = set()
         for i in nums:
-            result[i] = result[i]+1 if i in result else 1
-            if result[i]>1:
+            if i in hashset:
                 return True
+            hashset.add(i)
         return False
+        
+         # Count Number of times a number is in array
+#         result = {}
+#         for i in nums:
+#             result[i] = result[i]+1 if i in result else 1
+#             if result[i]>1:
+#                 return True
+#         return False
         
         
