6396d2369afb50d2a70555dc6deb8400cd7941d2
drednor <42916508+drednor@users.noreply.github.com>
2022-06-20 19:47:41 +0530

    Time: 114 ms (46.31%), Space: 15 MB (64.63%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        somemap ={}
+        somemap = {}
         for i , n in enumerate(nums):
-            if (target-n) in somemap:
+            if target-n in somemap:
                 return [somemap[target-n],i]
             somemap[n] = i
         return
-        
