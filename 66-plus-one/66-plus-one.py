d29889598c49d48e1765e60202f3bc6dc654eddc
drednor <42916508+drednor@users.noreply.github.com>
2022-06-21 01:01:15 +0530

    Time: 45 ms (57.89%), Space: 13.8 MB (58.03%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,10 +1,27 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        s = ""
-        for i in digits:
-            s += str(i)
-        temp = int(s)
-        temp += 1
-        temp = str(temp)
-        result = map(int, temp)
-        return result
+        carry , i = 1 , len(digits)- 1
+        while carry:
+            if i >= 0:
+                if digits[i] == 9:
+                    digits[i] = 0 
+                else:
+                    digits[i] += 1
+                    carry = 0
+            else:
+                digits.insert(0,1)
+                carry = 0
+            i-=1
+        return digits
+            
+        
+        
+        #simple solution
+        # s = ""
+        # for i in digits:
+        #     s += str(i)
+        # temp = int(s)
+        # temp += 1
+        # temp = str(temp)
+        # result = map(int, temp)
+        # return result
