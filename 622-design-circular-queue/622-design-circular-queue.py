bc0e497d331883ec613384d9ce08808f2d135a20
drednor <42916508+drednor@users.noreply.github.com>
2022-06-22 03:11:08 +0530

    Time: 132 ms (16.83%), Space: 15 MB (8.73%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.py
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -1,42 +1,49 @@
+class ListNode:
+    def __init__(self, val, nxt, prev):
+        self.val , self.next, self.prev = val, nxt, prev
+
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.queue = [None]*k
-        self.size = self.head = self.tail = 0
+        self.size = k
+        self.left = ListNode(0, None, None)
+        self.right = ListNode(0,None, self.left)
+        self.left.next = self.right
+        
 
     def enQueue(self, value: int) -> bool:
-        if self.isFull():
-            return False
-        self.queue[self.tail] = value
-        self.tail = (self.tail+1) % len(self.queue)
-        self.size +=1
+        if self.isFull():return False
+        cur = ListNode(value, self.right, self.right.prev)
+        self.right.prev.next = cur
+        self.right.prev = cur
+        self.size -= 1
         return True
         
 
     def deQueue(self) -> bool:
-        if self.isEmpty():
-            return False
-        self.head = (self.head + 1) % len(self.queue)
-        self.size-= 1
+        if self.isEmpty():return False
+        cur = self.left.next
+        self.left.next = self.left.next.next
+        self.left.next.prev = self.left
+        self.size += 1
         return True
-    
-    def Front(self) -> int:
-        if self.isEmpty():
-            return -1
-        return self.queue[self.head]
+        
 
+    def Front(self) -> int:
+        if self.isEmpty():return -1
+        return self.left.next.val
 
     def Rear(self) -> int:
-        if self.isEmpty():
-            return -1
-        return self.queue[self.tail-1]
+        if self.isEmpty(): return -1
+        return self.right.prev.val
+        
 
     def isEmpty(self) -> bool:
-        return self.size == 0
+        return self.left.next == self.right
+        
 
     def isFull(self) -> bool:
-        return self.size == len(self.queue)
-        
+        return self.size == 0
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
