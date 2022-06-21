fa7eb8fbcaf1853d99b9721c4337119a33ac7999
drednor <42916508+drednor@users.noreply.github.com>
2022-06-22 05:06:56 +0530

    Time: 113 ms (68.89%), Space: 20.7 MB (97.17%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,11 +1,23 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        result = []
-        for i in range(n+1):
-            res = 0
-            while i:
-                i &= i-1
-                res +=1
-            result.append(res) 
-        return result 
+        result = [0] *(n+1)
+        offset = 1
+        for i in range(1, n+1):
+            if offset * 2 == i:
+                offset = i
+            result[i] = 1 + result[i - offset]
+        return result
+        
+        
+        # Just Count the number of ones
+        # foreach value of i and append
+        # it to the result time complexity - O(nlogn) 
+        # result = []
+        # for i in range(n+1):
+        #     res = 0
+        #     while i:
+        #         i &= i-1
+        #         res +=1
+        #     result.append(res) 
+        # return result 
         
