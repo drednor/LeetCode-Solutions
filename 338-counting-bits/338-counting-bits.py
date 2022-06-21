88b660e18b2247ab77bb3a8812b8381253a10e7e
drednor <42916508+drednor@users.noreply.github.com>
2022-06-22 05:24:41 +0530

    Time: 173 ms (31.49%), Space: 20.9 MB (28.49%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -7,6 +7,16 @@ class Solution:
                 offset = i
             result[i] = 1 + result[i - offset]
         return result
+    
+        # Sample Input - n = 8
+        # Iteration 1 ---> 0b0001 result[1] = 1 + result[1 - 1] | Result right now = [0, 1, 0, 0, 0, 0, 0, 0, 0]
+        # Iteration 2 ---> 0b0010 result[2] = 1 + result[2 - 2] | Result right now = [0, 1, 1, 0, 0, 0, 0, 0, 0]
+        # Iteration 3 ---> 0b0011 result[3] = 1 + result[3 - 2] | Result right now = [0, 1, 1, 2, 0, 0, 0, 0, 0]
+        # Iteration 4 ---> 0b0100 result[4] = 1 + result[4 - 4] | Result right now = [0, 1, 1, 2, 1, 0, 0, 0, 0]
+        # Iteration 5 ---> 0b0101 result[5] = 1 + result[5 - 4] | Result right now = [0, 1, 1, 2, 1, 2, 0, 0, 0]
+        # Iteration 6 ---> 0b0110 result[6] = 1 + result[6 - 4] | Result right now = [0, 1, 1, 2, 1, 2, 2, 0, 0]
+        # Iteration 7 ---> 0b0111 result[7] = 1 + result[7 - 4] | Result right now = [0, 1, 1, 2, 1, 2, 2, 3, 0]
+        # Iteration 8 ---> 0b1000 result[8] = 1 + result[8 - 8] | Result right now = [0, 1, 1, 2, 1, 2, 2, 3, 1]
         
         
         # Just Count the number of ones
