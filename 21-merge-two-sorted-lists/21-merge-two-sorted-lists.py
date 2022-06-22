df673cf274c2384175d5d48b949ebec00a69c7d6
drednor <42916508+drednor@users.noreply.github.com>
2022-06-23 03:16:32 +0530

    Time: 45 ms (75.63%), Space: 13.8 MB (78.51%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,26 +5,47 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None and list2 is None:
-            return None
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
         dummy = result = ListNode(0)
-        a = list1
-        b =  list2
-        while a and b:
-            if a.val == b.val:
-                result.next = a
-                a = a.next
-            elif a.val<b.val:
-                result.next = a
-                a = a.next
+        while list1 and list2:
+            if list1.val == list2.val:
+                result.next = list1
+                list1 = list1.next
+            elif list1.val < list2.val:
+                result.next = list1
+                list1 = list1.next
             else:
-                result.next = b
-                b = b.next
+                result.next = list2
+                list2 = list2.next
             result = result.next
-        result.next = a or b
-        
+        result.next = list1 or list2
         return dummy.next
+                
+            
+        
+        
+        
+        
+        
+#         if list1 is None and list2 is None:
+#             return None
+#         if list1 is None:
+#             return list2
+#         if list2 is None:
+#             return list1
+#         dummy = result = ListNode(0)
+#         a = list1
+#         b =  list2
+#         while a and b:
+#             if a.val == b.val:
+#                 result.next = a
+#                 a = a.next
+#             elif a.val<b.val:
+#                 result.next = a
+#                 a = a.next
+#             else:
+#                 result.next = b
+#                 b = b.next
+#             result = result.next
+#         result.next = a or b
+        
+#         return dummy.next
