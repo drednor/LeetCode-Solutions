0b017c2873a95041b157f84f2553bf43aaa7ae2d
drednor <42916508+drednor@users.noreply.github.com>
2022-06-30 15:45:04 +0530

    Time: 115 ms (35.60%), Space: 13.9 MB (86.21%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -8,7 +8,7 @@ class Solution:
         dummy = cur = ListNode(0)
         carry = 0
         
-        while  l1 or l2 or carry:
+        while l1 or l2 or carry:
             if l1:
                 carry += l1.val
                 l1 = l1.next
@@ -19,3 +19,24 @@ class Solution:
             cur = cur.next
             carry //= 10
         return dummy.next
+        
+
+        
+        
+        
+        
+        
+#         dummy = cur = ListNode(0)
+#         carry = 0
+        
+#         while  l1 or l2 or carry:
+#             if l1:
+#                 carry += l1.val
+#                 l1 = l1.next
+#             if l2:
+#                 carry += l2.val
+#                 l2 = l2.next
+#             cur.next = ListNode(carry%10)
+#             cur = cur.next
+#             carry //= 10
+#         return dummy.next
