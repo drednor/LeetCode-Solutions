9600166d9a9b05b1bd1243fe318b509b726e7f15
drednor <42916508+drednor@users.noreply.github.com>
2022-07-01 18:55:59 +0530

    Time: 66 ms (43.42%), Space: 15.3 MB (80.90%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -6,21 +6,33 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # ITERATIVE BFS USING DEQUE
-        if not root:
-            return 0
+        #ITERATIVE DFS USING STACK
+        stack = [[root, 1]]
+        res = 0
+        while stack:
+            node , depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.right, depth+1])
+                stack.append([node.left, depth+1])
+        return res
         
-        level = 0 
-        q = deque([root])
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1
-        return level
+        
+        # ITERATIVE BFS USING DEQUE
+#         if not root:
+#             return 0
+        
+#         level = 0 
+#         q = deque([root])
+#         while q:
+#             for i in range(len(q)):
+#                 node = q.popleft()
+#                 if node.left:
+#                     q.append(node.left)
+#                 if node.right:
+#                     q.append(node.right)
+#             level += 1
+#         return level
         
         
         
