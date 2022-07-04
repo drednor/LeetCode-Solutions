6a1bd5734bf3ccd33e613ee5e58d5fccdcf33ed3
drednor <42916508+drednor@users.noreply.github.com>
2022-07-04 19:51:26 +0530

    Time: 48 ms (44.97%), Space: 13.9 MB (29.42%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -8,32 +8,33 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if p is None and q is None:
             return True
-        if p is None or q is None:
+        if p is None or q is None or p.val != q.val:
             return False
-        que = deque()
-        que.append((p,q))
-        while que:
-            first, second = que.popleft()
-            if first.val != second.val:
-                return False
-            if first.right:
-                if second.right:
-                    que.append((first.right,second.right))
-                else:
-                    return False
-            if second.right:
-                if first.right:
-                    pass
-                else:
-                    return False
-            if first.left:
-                if second.left:
-                    que.append((first.left,second.left))
-                else:
-                    return False
-            if second.left:
-                if first.left:
-                    pass
-                else:
-                    return False
-        return True
+        return (self.isSameTree(p.left, q.left)and self.isSameTree(p.right, q.right))
+        # que = deque()
+        # que.append((p,q))
+        # while que:
+        #     first, second = que.popleft()
+        #     if first.val != second.val:
+        #         return False
+        #     if first.right:
+        #         if second.right:
+        #             que.append((first.right,second.right))
+        #         else:
+        #             return False
+        #     if second.right:
+        #         if first.right:
+        #             pass
+        #         else:
+        #             return False
+        #     if first.left:
+        #         if second.left:
+        #             que.append((first.left,second.left))
+        #         else:
+        #             return False
+        #     if second.left:
+        #         if first.left:
+        #             pass
+        #         else:
+        #             return False
+        # return True
