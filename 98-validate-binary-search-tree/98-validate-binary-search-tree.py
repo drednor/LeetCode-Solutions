f86b19f3968ffecdb1231559f194a5addc89b887
drednor <42916508+drednor@users.noreply.github.com>
2022-07-05 12:55:59 +0530

    Time: 88 ms (17.45%), Space: 16.5 MB (44.99%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -10,9 +10,9 @@ class Solution:
         def valid(node, left, right):
             if node is None:
                 return True
-            if not (node.val > left and node.val < right):
+            if not(node.val > left and node.val< right):
                 return False
-            return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
+            return valid(node.left,left,node.val) and valid(node.right, node.val , right)
         return valid(root, float("-inf"), float("inf"))
 #         if root is None:
 #             return True
