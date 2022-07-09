49c5b537303e4b5defd3c5267888bc960bf9a564
drednor <42916508+drednor@users.noreply.github.com>
2022-07-09 22:01:22 +0530

    Time: 47 ms (46.73%), Space: 13.9 MB (30.29%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        def ph(s,sub, result):
+            if len(s) == 0:
+                result.append(sub)
+                return
+            char = s[0]
+            digit = int(char)
+            if char == "7" or char == "9" or char == "8":
+                start = (digit-2)*3
+                if char == "8":
+                    start += 1
+                    end = start+3
+                elif char == "9":
+                    start += 1
+                    end = start+4
+                else:
+                    end = start + 4
+            else:
+                start = (digit-2)*3
+                end = start+3
+            for i in range(start,end):
+                temp = chr(ord("a") + i)
+                ph(s[1:],sub+temp,result)
+            return result
+        return ph(digits,"",[])
