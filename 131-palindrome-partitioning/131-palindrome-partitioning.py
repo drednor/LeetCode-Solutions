929b65023af851de012c3c25da8d4ad9e6a20490
drednor <42916508+drednor@users.noreply.github.com>
2022-07-11 23:31:26 +0530

    Time: 1267 ms (14.59%), Space: 30.4 MB (24.56%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        def backtrack(i, sub):
+            if i >= len(s):
+                result.append(sub.copy())
+                return
+            for j in range(i,len(s)):
+                if self.ispal(s[i:j+1]):
+                    backtrack(j+1,sub+[s[i:j+1]])
+            
+        backtrack(0,[])
+        return result
+        
+    def ispal(self, s):
+        return s ==s[::-1]
