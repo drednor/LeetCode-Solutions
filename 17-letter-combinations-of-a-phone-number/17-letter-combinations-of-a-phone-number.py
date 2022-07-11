3540a3ae9aaf6fed3873c80885d13ba09489e581
drednor <42916508+drednor@users.noreply.github.com>
2022-07-12 00:14:03 +0530

    Time: 55 ms (28.77%), Space: 13.9 MB (30.81%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,27 +1,52 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
-        def ph(s,sub, result):
-            if len(s) == 0:
+        def ph(nums, sub,result):
+            if len(nums) == 0:
                 result.append(sub)
                 return
-            char = s[0]
-            digit = int(char)
-            if char >= "7":
-                start = (digit-2)*3
-                if char == "8":
+            temp = int(nums[0])
+            if temp >= 7:
+                start = (temp-2)*3
+                if temp == 8:
                     start += 1
                     end = start+3
-                elif char == "9":
+                elif temp == 9:
                     start += 1
                     end = start+4
                 else:
                     end = start + 4
             else:
-                start = (digit-2)*3
+                start = (temp-2)*3
                 end = start+3
-            for i in range(start,end):
-                temp = chr(ord("a") + i)
-                ph(s[1:],sub+temp,result)
+            for i in range(start, end):
+                char = chr(ord("a") + i)
+                ph(nums[1:],sub+char,result)
             return result
-        return ph(digits,"",[])
+        return ph(digits, "",[])
+    
+        
+
+        # def ph(s,sub, result):
+        #     if len(s) == 0:
+        #         result.append(sub)
+        #         return
+        #     char = s[0]
+        #     digit = int(char)
+        #     if char >= "7":
+        #         start = (digit-2)*3
+        #         if char == "8":
+        #             start += 1
+        #             end = start+3
+        #         elif char == "9":
+        #             start += 1
+        #             end = start+4
+        #         else:
+        #             end = start + 4
+        #     else:
+        #         start = (digit-2)*3
+        #         end = start+3
+        #     for i in range(start,end):
+        #         temp = chr(ord("a") + i)
+        #         ph(s[1:],sub+temp,result)
+        #     return result
+        # return ph(digits,"",[])
