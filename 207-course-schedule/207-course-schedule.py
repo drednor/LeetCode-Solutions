25d67e3485ce9e356b2c425f0af96e28c614862b
drednor <42916508+drednor@users.noreply.github.com>
2022-07-14 22:18:41 +0530

    Time: 104 ms (91.96%), Space: 17.1 MB (25.63%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,10 +1,9 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = {i:[] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            graph[crs].append(pre)
+        for course, pre in prerequisites:
+            graph[course].append(pre)
         visited= set()
-        
         def dfs(crs):
             if crs in visited:
                 return False
@@ -12,12 +11,40 @@ class Solution:
                 return True
             visited.add(crs)
             for pre in graph[crs]:
-                if not dfs(pre): return False
+                if not dfs(pre):
+                    return False
             visited.remove(crs)
             graph[crs] = []
             return True
         
         for crs in range(numCourses):
-            if not dfs(crs): return False
+            if not dfs(crs):
+                return False
         return True
+
+        
+        
+        
+        
+        
+        #         graph = {i:[] for i in range(numCourses)}
+#         for crs, pre in prerequisites:
+#             graph[crs].append(pre)
+#         visited= set()
+        
+#         def dfs(crs):
+#             if crs in visited:
+#                 return False
+#             if graph[crs] == []:
+#                 return True
+#             visited.add(crs)
+#             for pre in graph[crs]:
+#                 if not dfs(pre): return False
+#             visited.remove(crs)
+#             graph[crs] = []
+#             return True
+        
+#         for crs in range(numCourses):
+#             if not dfs(crs): return False
+#         return True
         
