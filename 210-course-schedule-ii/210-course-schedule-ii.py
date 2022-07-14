57ab2e82675003431f8aa0fb5c42804e3e1df21f
drednor <42916508+drednor@users.noreply.github.com>
2022-07-14 23:23:58 +0530

    Time: 222 ms (15.17%), Space: 17.7 MB (11.46%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            graph[crs].append(pre)
+        visited = set()
+        cycle = set()
+        result = []
+        
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visited:
+                return True
+            if graph[crs] == []:
+                if crs not in result:
+                    result.append(crs)
+                return True
+            cycle.add(crs)
+            for pre in graph[crs]:
+                if not dfs(pre):
+                    return False
+            cycle.remove(crs)
+            visited.add(crs)
+            if crs not in result:
+                result.append(crs)
+            graph[crs] == []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return []
+        return result
