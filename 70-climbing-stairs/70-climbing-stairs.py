85db6109d7f0d8a093eee9824e05a69371a8fbfb
drednor <42916508+drednor@users.noreply.github.com>
2022-07-21 13:53:52 +0530

    Time: 43 ms (58.44%), Space: 14 MB (11.65%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def recur(x):
+            if x in memo:
+                return memo[x]
+            if x == 0:
+                return 1
+            if x<0:
+                return 0
+            memo[x] = recur(x-1) + recur(x-2)
+            return memo[x]
+        return recur(n)
+        
