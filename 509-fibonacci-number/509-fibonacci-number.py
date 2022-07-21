f67a8a2f1b862a29e232acf7bce6e9a55d2be58f
drednor <42916508+drednor@users.noreply.github.com>
2022-07-21 14:21:14 +0530

    Time: 49 ms (58.64%), Space: 13.8 MB (53.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,13 +1,24 @@
 class Solution:
     def fib(self, n: int) -> int:
-        memo ={}
-        def recur(x):
-            if x in memo:
-                return memo[x]
-            if x == 1:
-                return 1
-            if x == 0:
-                return 0
-            memo[x] = recur(x-1) + recur(x-2)
-            return memo[x]
-        return recur(n)
+        if n == 0:
+            return 0
+        table = [None]*(n+1)
+        table[0] = 0
+        table[1] = 1
+        for i in range(len(table)):
+            if i+2 <len(table):
+                table[i+2] = table[i] + table[i+1]
+        return table[n]
+                    
+                                            
+        # memo ={}
+        # def recur(x):
+        #     if x in memo:
+        #         return memo[x]
+        #     if x == 1:
+        #         return 1
+        #     if x == 0:
+        #         return 0
+        #     memo[x] = recur(x-1) + recur(x-2)
+        #     return memo[x]
+        # return recur(n)
