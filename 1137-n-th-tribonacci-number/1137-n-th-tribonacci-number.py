eeed83a6bc82cca3718d9c3db836cacd76573306
drednor <42916508+drednor@users.noreply.github.com>
2022-07-21 14:29:58 +0530

    Time: 47 ms (46.36%), Space: 13.8 MB (96.33%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,15 +1,19 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return n
-        if n == 1:
-            return 1
-        table = [None] * (n+1)
-        table[0] = 0
-        table[1] = 1
-        table[2] = 1
-        for i in range(len(table)):
-            if i+3<len(table):
-                table[i+3] = table[i+2] + table[i+1] + table[i]
-        return table[n]
+        a, b, c = 0, 1, 1
+        for _ in range(n):
+            a, b, c = b, c, a+b+c
+        return a
+        # if n == 0:
+        #     return n
+        # if n == 1:
+        #     return 1
+        # table = [None] * (n+1)
+        # table[0] = 0
+        # table[1] = 1
+        # table[2] = 1
+        # for i in range(len(table)):
+        #     if i+3<len(table):
+        #         table[i+3] = table[i+2] + table[i+1] + table[i]
+        # return table[n]
         
