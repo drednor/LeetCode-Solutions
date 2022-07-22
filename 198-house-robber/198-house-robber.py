da2bc31b55e65d4a9657ea9084afc2e5f3331369
drednor <42916508+drednor@users.noreply.github.com>
2022-07-22 18:41:43 +0530

    Time: 63 ms (16.29%), Space: 13.9 MB (19.08%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,13 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
-        def recur(i):
-            if i in memo:
-                return memo[i]
-            if i >= len(nums):
-                return 0
-            left = nums[i] + recur(i+2)
-            right = recur(i+1)
-            memo[i] = max(left,right)
-            return memo[i]
-        return recur(0)
+        nums.insert(0,0)
+        for i in range(len(nums)):
+            if i+3<len(nums):
+                nums[i+3] += max(nums[i],nums[i+1])
+        return max(nums[-1],nums[-2])
+        
+        
+        
+        # memo = {}
+        # def recur(i):
+        #     if i in memo:
+        #         return memo[i]
+        #     if i >= len(nums):
+        #         return 0
+        #     left = nums[i] + recur(i+2)
+        #     right = recur(i+1)
+        #     memo[i] = max(left,right)
+        #     return memo[i]
+        # return recur(0)
