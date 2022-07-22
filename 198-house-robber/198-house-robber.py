bb364b86d90ff51a1b21f1aa79e35ba12ef785f0
drednor <42916508+drednor@users.noreply.github.com>
2022-07-22 18:16:43 +0530

    Time: 40 ms (74.20%), Space: 14 MB (19.08%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,7 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 2:
-            return max(nums[0],nums[1])
         memo = {}
         def recur(i):
             if i in memo:
