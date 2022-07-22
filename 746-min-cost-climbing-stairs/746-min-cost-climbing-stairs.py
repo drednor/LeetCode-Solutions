93db0a6d1d96d809b8942775d7490f88dc4014db
drednor <42916508+drednor@users.noreply.github.com>
2022-07-22 17:38:11 +0530

    Time: 103 ms (36.12%), Space: 14.1 MB (20.70%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -3,22 +3,22 @@ class Solution:
         # table = [0] * (len(cost))
         # table[0] = cost[0]
         # table[1] = cost[1]
-        # for i in range(len(cost)):
-        #     if i+2<len(cost):
-        #         cost[i+2] += min(cost[i],cost[i+1])
-        # return min(cost[-1], cost[-2])
+        for i in range(len(cost)):
+            if i+2<len(cost):
+                cost[i+2] += min(cost[i],cost[i+1])
+        return min(cost[-1], cost[-2])
         
-        memo = {}
-        def recur(i):
-            if i in memo:
-                return memo[i]
-            if i == len(cost)-1:
-                return cost[-1]
-            if i == len(cost)-2:
-                return cost[-2]
-            memo[i] = cost[i] + min(recur(i+1) if i < (len(cost)-1) else 1000 ,recur(i+2) if i < (len(cost)-2) else 1000)
-            return memo[i]
-        return min(recur(0), recur(1))
+#         memo = {}
+#         def recur(i):
+#             if i in memo:
+#                 return memo[i]
+#             if i == len(cost)-1:
+#                 return cost[-1]
+#             if i == len(cost)-2:
+#                 return cost[-2]
+#             memo[i] = cost[i] + min(recur(i+1) if i < (len(cost)-1) else 1000 ,recur(i+2) if i < (len(cost)-2) else 1000)
+#             return memo[i]
+#         return min(recur(0), recur(1))
                 
              
             
