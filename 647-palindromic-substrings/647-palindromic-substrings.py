bad7dde72008910a642d938a7e739ac5370aa1d0
drednor <42916508+drednor@users.noreply.github.com>
2022-07-23 12:05:36 +0530

    Time: 221 ms (52.25%), Space: 13.7 MB (98.41%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,14 +1,9 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         res  = 0
-        flag = False
-        if len(s) % 2 == 1:
-            flag == True
         for i in range(len(s)):
             l , r = i , i
-            # if flag:
             while l >= 0 and r<len(s) and s[l] == s[r]:
-                # print("hello")
                 res += 1
                 l -= 1
                 r += 1
@@ -17,5 +12,4 @@ class Solution:
                 res+=1
                 l -= 1
                 r += 1
-
         return res
