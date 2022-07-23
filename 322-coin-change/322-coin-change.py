f4bb7f446ab56689fcb17f62b846429c4ae9a373
drednor <42916508+drednor@users.noreply.github.com>
2022-07-23 12:43:15 +0530

    Time: 958 ms (95.01%), Space: 14.2 MB (55.54%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,13 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        table = [None]* (amount+1)
+        table[0] = 0
+        for i in range(len(table)):
+            if table[i] is not None:
+                for coin in coins:
+                    temp = i + coin
+                    if temp< len(table):
+                        shortest = table[i] + 1
+                        if table[temp] is None or shortest < table[temp]:
+                            table[temp] = shortest
+        return table[amount] if table[amount] is not None else -1
