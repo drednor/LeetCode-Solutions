ac8acce0cfffb02b1742dd05dd4d0e93a8e1b3df
drednor <42916508+drednor@users.noreply.github.com>
2022-07-24 11:15:41 +0530

    Time: 1775 ms (70.46%), Space: 44.4 MB (5.50%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,13 +1,41 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        table = [None] * (amount+1)
-        table[0] = 0
-        for i in range(len(table)):
-            if table[i] is not None:
-                for coin in coins:
-                    temp = i + coin
-                    if temp < len(table):
-                        shortest = table[i] + 1
-                        if table[temp] is None or shortest < table[temp]:
-                            table[temp] = shortest
-        return table[amount] if table[amount] is not None else -1
+        memo = {}
+        def recur(total):
+            if total in memo:
+                return memo[total]
+            if total == 0:
+                return 0
+            if total < 0:
+                return float("inf")
+            shortest = float("inf")
+            for coin in coins:
+                rem = total - coin
+                res = 1 + recur(rem)
+                if res < shortest:
+                    shortest = res
+            memo[total] = shortest
+            return shortest
+        result = recur(amount)
+        if result == float("inf"):
+            return -1
+        else:
+            return result
+                    
+            
+        
+        
+        
+        
+        
+        # table = [None] * (amount+1)
+        # table[0] = 0
+        # for i in range(len(table)):
+        #     if table[i] is not None:
+        #         for coin in coins:
+        #             temp = i + coin
+        #             if temp < len(table):
+        #                 shortest = table[i] + 1
+        #                 if table[temp] is None or shortest < table[temp]:
+        #                     table[temp] = shortest
+        # return table[amount] if table[amount] is not None else -1
