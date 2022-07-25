ebc436b683e9622cbee70018ac35e50c0891807a
drednor <42916508+drednor@users.noreply.github.com>
2022-07-25 16:39:35 +0530

    Time: 72 ms (6.70%), Space: 14.2 MB (6.74%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,14 +1,77 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         memo = {}
-        def recur(r,c):
-            key = (r,c)
+        def recur(i,j):
+            key = (i,j)
             if key in memo:
                 return memo[key]
-            if r == 1 and c == 1:
+            if i == m-1 and j == n-1:
                 return 1
-            if r == 0 or c == 0:
+            if i > m-1 or j>n-1:
                 return 0
-            memo[key] = recur(r-1,c) + recur(r,c-1)
+            memo[key] = recur(i+1,j) + recur(i,j+1)
             return memo[key]
-        return recur(m,n)
+        return recur(0,0)
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # memo = {}
+        # def recur(r,c):
+        #     key = (r,c)
+        #     if key in memo:
+        #         return memo[key]
+        #     if r == 1 and c == 1:
+        #         return 1
+        #     if r == 0 or c == 0:
+        #         return 0
+        #     memo[key] = recur(r-1,c) + recur(r,c-1)
+        #     return memo[key]
+        # return recur(m,n)
