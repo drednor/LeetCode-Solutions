4a17d3fbda97001e379744ec423b4db12e94800b
drednor <42916508+drednor@users.noreply.github.com>
2022-07-25 11:40:21 +0530

    Time: 40 ms (92.31%), Space: 13.8 MB (94.68%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,16 +1,33 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        memo = {}
-        def recur(sub):
-            if sub in memo:
-                return memo[sub]
-            if len(sub) == 0:
-                return True
-            for word in wordDict:
-                if sub.startswith(word):
-                    memo[sub[len(word):]] = recur(sub[len(word):])
-                    if memo[sub[len(word):]]:
-                        return True
-            memo[sub] = False
-            return False
-        return recur(s)
+        table = [False] * (len(s)+1)
+        table[0] = True
+        for i in range(len(s)):
+            if table[i]:
+                for word in wordDict:
+                    #print(word)
+                    if s[i:].startswith(word):
+                        temp = i + len(word)
+                        if temp < len(table):
+                            table[temp] = True
+        #print(table)
+        return table[-1]
+        
+        
+        
+        
+        
+        # memo = {}
+        # def recur(sub):
+        #     if sub in memo:
+        #         return memo[sub]
+        #     if len(sub) == 0:
+        #         return True
+        #     for word in wordDict:
+        #         if sub.startswith(word):
+        #             memo[sub[len(word):]] = recur(sub[len(word):])
+        #             if memo[sub[len(word):]]:
+        #                 return True
+        #     memo[sub] = False
+        #     return False
+        # return recur(s)
