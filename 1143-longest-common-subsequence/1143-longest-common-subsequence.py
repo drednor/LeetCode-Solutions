3c6930191a257a608b343526cc2aaef4ec918148
drednor <42916508+drednor@users.noreply.github.com>
2022-07-26 12:37:01 +0530

    Time: 1923 ms (7.47%), Space: 140 MB (15.21%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = {}
+        def recur(i,j):
+            key = (i,j)
+            if key in memo:
+                return memo[key]
+            if i == len(text1) or j == len(text2):
+                return 0
+            elif text1[i] == text2[j]:
+                memo[key] = 1 + recur(i+1,j+1)
+            else :
+                memo[key] = max(recur(i+1,j),recur(i,j+1))
+            return memo[key]
+        return recur(0,0)
+            
+        
+        
+        
+        
