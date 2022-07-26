cdc2d58009e09b914e636bcc59c43edf35e92ff6
drednor <42916508+drednor@users.noreply.github.com>
2022-07-26 13:15:06 +0530

    Time: 1259 ms (92.10%), Space: 14.3 MB (41.46%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,32 +1,92 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
-        def recur(total):
-            if total in memo:
-                return memo[total]
-            if total == 0:
-                return 0
-            if total < 0:
-                return float("inf")
-            shortest = float("inf")
-            for coin in coins:
-                rem = total - coin
-                res = 1 + recur(rem)
-                if res < shortest:
-                    shortest = res
-            memo[total] = shortest
-            return shortest
-        result = recur(amount)
-        if result == float("inf"):
-            return -1
-        else:
-            return result
+        table = [None] * (amount+1)
+        table[0] = 0
+        for i in range(len(table)):
+            if table[i] is not None:
+                for coin in coins:
+                    temp = i + coin
+                    if temp < len(table):
+                        shortest = 1 + table[i]
+                        if table[temp] is None or shortest < table[temp]:
+                            table[temp] = shortest
+        return table[-1] if table[-1] is not None else -1
+                        
                     
-            
         
         
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         memo = {}
+#         def recur(total):
+#             if total in memo:
+#                 return memo[total]
+#             if total == 0:
+#                 return 0
+#             if total < 0:
+#                 return float("inf")
+#             shortest = float("inf")
+#             for coin in coins:
+#                 rem = total - coin
+#                 res = 1 + recur(rem)
+#                 if res < shortest:
+#                     #print(f"res = {res}, shortest = {shortest}, total = {total}, coin = {coin}")
+#                     shortest = res
+#             memo[total] = shortest
+#             return shortest
+#         result = recur(amount)
+#         if result == float("inf"):
+#             return -1
+#         else:
+#             return result
+
         
         # table = [None] * (amount+1)
         # table[0] = 0
