c39808a9a10c5a459eff43784567d29207eca79a
drednor <42916508+drednor@users.noreply.github.com>
2022-07-26 12:58:22 +0530

    Time: 402 ms (92.36%), Space: 22 MB (76.16%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,19 +1,30 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        table = [[0]*(len(text1)+1) for _ in range(len(text2)+1)]
+        for i in range(len(text2)):
+            for j in range(len(text1)):
+                if text1[j] == text2[i]:
+                    table[i+1][j+1] = 1 + table[i][j]
+                else:
+                    table[i+1][j+1] = max(table[i+1][j],table[i][j+1])
+        return table[-1][-1]
         
-        memo = {}
-        def recur(i,j):
-            key = (i,j)
-            if key in memo:
-                return memo[key]
-            if i == len(text1) or j == len(text2):
-                return 0
-            elif text1[i] == text2[j]:
-                memo[key] = 1 + recur(i+1,j+1)
-            else :
-                memo[key] = max(recur(i+1,j),recur(i,j+1))
-            return memo[key]
-        return recur(0,0)
+        
+        
+        
+#         memo = {}
+#         def recur(i,j):
+#             key = (i,j)
+#             if key in memo:
+#                 return memo[key]
+#             if i == len(text1) or j == len(text2):
+#                 return 0
+#             elif text1[i] == text2[j]:
+#                 memo[key] = 1 + recur(i+1,j+1)
+#             else :
+#                 memo[key] = max(recur(i+1,j),recur(i,j+1))
+#             return memo[key]
+#         return recur(0,0)
             
         
         
