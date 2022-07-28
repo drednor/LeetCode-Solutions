1948cddf4effd8c8530f54671903acb36bf61645
drednor <42916508+drednor@users.noreply.github.com>
2022-07-28 11:45:49 +0530

    Time: 666 ms (47.21%), Space: 18.2 MB (38.54%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -3,7 +3,7 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
         visited = {}
-        self.res = -1
+        
         def dfs(r,c, prev):
             if 0<=r<rows and 0<=c<cols and matrix[r][c] > prev:
                 if (r,c) in visited:
@@ -14,13 +14,12 @@ class Solution:
                 count = max(count, 1 + dfs(r-1,c,matrix[r][c]))
                 count = max(count, 1 + dfs(r,c-1,matrix[r][c]))
                 visited[(r,c)] = count
-                self.res = max(count, self.res)
                 return count
             return 0
         
         for r in range(rows):
             for c in range(cols):
                 dfs(r,c,-1)
-        return self.res
+        return max(visited.values())
                             
                             
