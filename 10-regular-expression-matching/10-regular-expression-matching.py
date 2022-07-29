ec49a834b17ba2ef0c7adfd16f0cb50ced8cf33b
drednor <42916508+drednor@users.noreply.github.com>
2022-07-29 22:25:18 +0530

    Time: 2698 ms (5.01%), Space: 14.2 MB (27.24%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+        def recur(i,j):
+            key = (i,j)
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+            
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+            if j+1<len(p) and p[j+1] == "*":
+                memo[key] = recur(i,j+2) or (match and recur(i+1,j))
+                return memo[key]
+            if match:
+                memo[key] = recur(i+1,j+1)
+                return memo[key]
+            memo[key] = False
+            return memo[key]                
+        return recur(0,0)
