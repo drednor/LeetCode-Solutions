bc8274c8a2f5fde70b1fa06ddbb3fdba317d163d
drednor <42916508+drednor@users.noreply.github.com>
2022-08-01 18:28:19 +0530

    Time: 41 ms (73.81%), Space: 14 MB (14.98%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,17 +1,28 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
-        def dfs(i,j):
-            key = (i,j)
-            if key in memo:
-                return memo[key]
-            if i == 1 and j == 1:
-                return 1
-            if i == 0 or j == 0:
-                return 0
-            memo[key] = dfs(i-1,j) + dfs(i, j-1)
-            return memo[key]
-        return dfs(m,n)
+        table = [[0] * (n+1) for _ in range(m+1)]
+        table[1][1] = 1
+        for i in range(len(table)):
+            for j in range(len(table[0])):
+                if i-1>=0:
+                    table[i][j] += table[i-1][j]
+                if j-1>=0:
+                    table[i][j] += table[i][j-1]
+        return table[-1][-1]
+   
+        
+        # memo = {}
+        # def dfs(i,j):
+        #     key = (i,j)
+        #     if key in memo:
+        #         return memo[key]
+        #     if i == 1 and j == 1:
+        #         return 1
+        #     if i == 0 or j == 0:
+        #         return 0
+        #     memo[key] = dfs(i-1,j) + dfs(i, j-1)
+        #     return memo[key]
+        # return dfs(m,n)
         
         
         
