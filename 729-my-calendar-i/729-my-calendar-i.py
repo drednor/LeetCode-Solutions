b2d6fc2cbe633ac91ebbd3d489b4504f8572849d
drednor <42916508+drednor@users.noreply.github.com>
2022-08-03 17:52:16 +0530

    Time: 419 ms (56.93%), Space: 14.7 MB (92.29%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -2,23 +2,38 @@ class MyCalendar:
 
     def __init__(self):
         # self.res = set()
-        self.res = []
+        self.res = [(0,0),(10**9,10**9)]
         
     def book(self, start: int, end: int) -> bool:
-        if len(self.res) == 0:
-            self.res.append((start,end))
-            return True
-        for i in range(len(self.res)):
-            if self.res[i][0] <= start < end <= self.res[i][1]:
-                return False
-            if start <= self.res[i][0] < end <= self.res[i][1]:
-                return False
-            if self.res[i][0] <= start < self.res[i][1] < end:
-                return False
-            if start <= self.res[i][0] < self.res[i][1] < end:
-                return False
-        self.res.append((start,end))
+        l , r = 0, len(self.res)
+        while l < r:
+            mid = (l+r)//2
+            if self.res[mid][0] == end:
+                l = mid
+                break
+            elif self.res[mid][0] < end:
+                l = mid + 1
+            else:
+                r = mid
+        if start<self.res[l-1][1]: return False
+        self.res.insert(l,(start,end))
         return True
+        
+        
+        # if len(self.res) == 0:
+        #     self.res.append((start,end))
+        #     return True
+        # for i in range(len(self.res)):
+        #     if self.res[i][0] <= start < end <= self.res[i][1]:
+        #         return False
+        #     if start <= self.res[i][0] < end <= self.res[i][1]:
+        #         return False
+        #     if self.res[i][0] <= start < self.res[i][1] < end:
+        #         return False
+        #     if start <= self.res[i][0] < self.res[i][1] < end:
+        #         return False
+        # self.res.append((start,end))
+        # return True
         
         
         
