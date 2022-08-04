5e0d67682eb6c74f09361264243622668b04b1a3
drednor <42916508+drednor@users.noreply.github.com>
2022-08-04 19:11:00 +0530

    Time: 62 ms (15.84%), Space: 13.8 MB (63.37%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.py
+++ 858-mirror-reflection/858-mirror-reflection.py
@@ -1,17 +1,10 @@
 class Solution:
     def mirrorReflection(self, p: int, q: int) -> int:
-        if q == p:
+        if p == q:
             return 1
-        # print(q/p)
-        # if (q/p) > 0.5:
-        #     res 
-        # print(q)
-        # temp = q
-        
         while p%2 ==0 and q %2==0:
             p/=2
             q/=2
-        
         if p%2 == 0 and q%2 == 1:
             return 2
         elif p%2 == 1 and q%2 == 0:
@@ -19,14 +12,3 @@ class Solution:
         elif p%2 == 1 and q%2 ==1:
             return 1
         return -1
-#         while temp <= p:
-#             if temp == p:
-#                 return 2
-#             temp *= 2
-        
-#         temp = q
-#         while temp <= p:
-#             if temp == p:
-#                 return 1
-#             temp *= 3
-#         return 0
