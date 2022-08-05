ab076f056bf523cb5fe465d0b9d35477eb77efdf
drednor <42916508+drednor@users.noreply.github.com>
2022-08-05 20:08:55 +0530

    Time: 68 ms (42.39%), Space: 14 MB (29.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -10,22 +10,31 @@ class Solution:
         #                 table[i] += table[temp]
         # #print(table)
         # return table[target]
-        
-        
-        memo = {}
-        def recur(total):
-            key = total
-            if key in memo:
-                return memo[key]
-            if total < 0:
-                return 0
-            count = 0
-            for num in nums:
-                if total > num:
-                    count += recur(total - num)
-                if total == num:
-                    count += 1
-            memo[key] = count
-            return memo[key]
-        return recur(target)
+    
+        table = [0] * (target +1)
+        table[0] = 1
+        for i in range(len(table)):
+            if table[i] != 0:
+                for num in nums:
+                    temp = i + num
+                    if temp < len(table):
+                        table[temp] += table[i]
+        return table[target]
+                
+        # memo = {}
+        # def recur(total):
+        #     key = total
+        #     if key in memo:
+        #         return memo[key]
+        #     if total < 0:
+        #         return 0
+        #     count = 0
+        #     for num in nums:
+        #         if total > num:
+        #             count += recur(total - num)
+        #         if total == num:
+        #             count += 1
+        #     memo[key] = count
+        #     return memo[key]
+        # return recur(target)
             
