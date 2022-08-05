90374fab1ca08a7ae06734ed14dcc887077437a7
drednor <42916508+drednor@users.noreply.github.com>
2022-08-05 19:55:34 +0530

    Time: 50 ms (78.45%), Space: 14.2 MB (18.29%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,34 +1,31 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        table = [0] * (target+1)
-        table[0] = 1
-        for i in range(len(table)):
-            if table[i] == 0:
-                for num in nums:
-                    temp = i - num
-                    if temp>=0:
-                        table[i] += table[temp]
-        #print(table)
-        return table[target]
+        # table = [0] * (target+1)
+        # table[0] = 1
+        # for i in range(len(table)):
+        #     if table[i] == 0:
+        #         for num in nums:
+        #             temp = i - num
+        #             if temp>=0:
+        #                 table[i] += table[temp]
+        # #print(table)
+        # return table[target]
         
         
-        # memo = {}
-        # self.res = 0
-        # def recur(total):
-        #     key = total
-        #     if key in memo:
-        #         return memo[key]
-        #     if total == 0:
-        #         #print(sub)
-        #         return 1
-        #     if total < 0:
-        #         return 0
-        #     for num in nums:
-        #         rem = total - num
-        #         memo[key] = recur(rem, sub + tuple([num]))
-        #         if memo[key]:
-        #             self.res += 1
-        #     return memo[key]
-        # recur(target,())
-        # return self.res
+        memo = {}
+        def recur(total):
+            key = total
+            if key in memo:
+                return memo[key]
+            if total < 0:
+                return 0
+            count = 0
+            for num in nums:
+                if total > num:
+                    count += recur(total - num)
+                if total == num:
+                    count += 1
+            memo[key] = count
+            return memo[key]
+        return recur(target)
             
