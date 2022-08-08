f9b57ffb51610bab2a6f4d03d8b85b32dc49eb30
drednor <42916508+drednor@users.noreply.github.com>
2022-08-09 03:57:46 +0530

    Time: 64 ms (27.38%), Space: 14.1 MB (14.36%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,11 +1,89 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
-        while n!=1:
-            n = sum([int(i)**2 for i in str(n)])
-            if n in seen:
+        s = set()
+        def recur(num):
+            if num == 1:
+                return True
+            if num in s:
                 return False
+            s.add(num)
+            total = 0
+            for char in str(num):
+                total += int(char)**2
+            if recur(total):
+                return True
             else:
-                seen.add(n)
-        else:
-            return True
+                return False
+            return False
+        return recur(n)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # seen = set()
+        # while n!=1:
+        #     n = sum([int(i)**2 for i in str(n)])
+        #     if n in seen:
+        #         return False
+        #     else:
+        #         seen.add(n)
+        # else:
+        #     return True
