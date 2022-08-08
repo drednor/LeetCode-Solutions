c1dbf52c4cd7b18776bcce63d0de65883c2a1f2d
drednor <42916508+drednor@users.noreply.github.com>
2022-08-08 19:30:41 +0530

    Time: 88 ms (94.55%), Space: 14.2 MB (47.45%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -5,10 +5,9 @@ class Solution:
             if len(table) == 0 or table[-1] < num:
                 table.append(num)
             else:
-                idx = bisect_left(table, num)
+                idx = bisect.bisect_left(table, num)
                 table[idx] = num
         return len(table)
-        
 #         table = [1] * len(nums)
 #         for i in range(len(nums)-1,-1,-1):
 #             for j in range(i+1, len(nums)):
