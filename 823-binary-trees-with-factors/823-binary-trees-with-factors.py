808ffcfb5fea55023605cc40e8fd135b7ca1d754
drednor <42916508+drednor@users.noreply.github.com>
2022-08-13 06:24:49 +0530

    Time: 2035 ms (11.61%), Space: 70.4 MB (5.05%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -5,8 +5,7 @@ class Solution:
         for root in arr:
             temp = 1
             for child in arr:
-                if child > root: break
-                temp += (memo[child]*memo[root/child])
+                if child>root : break
+                temp += (memo[child]*memo[(root/child)])
             memo[root] = temp
-        return sum(memo.values())%(10**9+7)
-            
+        return sum(memo.values()) % (10**9+7)
