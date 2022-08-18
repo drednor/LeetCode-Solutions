307f4416223442de3bc872fe028a5289f00f4ff4
drednor <42916508+drednor@users.noreply.github.com>
2022-08-18 11:06:50 +0530

    Time: 174 ms (92.96%), Space: 15.5 MB (34.89%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -2,16 +2,20 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # c = Counter(nums).most_common()
         # return c[0][0]
-        memo = {}
-        temp = [None, float("-inf")]
+        res, count = 0 , 0
         for num in nums:
-            memo[num] = 1 + memo[num] if num in memo else 1
-            val = memo[num]
-            if val > temp[1]:
-                temp = [num, val]
-        return temp[0]
-        # temp = [None,float("-inf")]
-        # for key, val in memo.items():
+            if count == 0:
+                res = num
+            count += (1 if res == num else -1)
+        return res
+        
+        
+        # memo = {}
+        # temp = [None, float("-inf")]
+        # for num in nums:
+        #     memo[num] = 1 + memo[num] if num in memo else 1
+        #     val = memo[num]
         #     if val > temp[1]:
-        #         temp = [key, val]
+        #         temp = [num, val]
         # return temp[0]
+        
