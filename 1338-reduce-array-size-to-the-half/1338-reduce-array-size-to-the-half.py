c010abfa9aa9862ad5bf9dcd5980576d9363ab1f
drednor <42916508+drednor@users.noreply.github.com>
2022-08-18 06:33:36 +0530

    Time: 623 ms (93.90%), Space: 31.9 MB (81.83%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,15 +1,32 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        memo = Counter(arr).most_common()
+        memo = {}
+        for num in arr:
+            memo[num] = 1+ memo[num] if num in memo else 1
         # print(memo)
+        temp = []
+        for key, val in memo.items():
+            temp.append(val)
+        temp.sort(reverse=True)
+        # print(temp)
+        res = 0
         total = len(arr)
-        # print(total)
-        temp = 0
-        res = float("inf")
-        for key, val in memo:
-            total -= val
-            temp += 1
-            # print(key, val, temp, total)
-            if total<= len(arr)//2:
+        for num in temp:
+            total -= num
+            res+=1
+            if total <= (len(arr)//2):
                 break
-        return temp
+        return res
+        # memo = Counter(arr).most_common()
+        # # print(memo)
+        # total = len(arr)
+        # # print(total)
+        # temp = 0
+        # res = float("inf")
+        # for key, val in memo:
+        #     total -= val
+        #     temp += 1
+        #     # print(key, val, temp, total)
+        #     if total<= len(arr)//2:
+        #         break
+        # return temp
