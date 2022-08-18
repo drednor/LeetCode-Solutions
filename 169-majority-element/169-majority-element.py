4ed5b6ca5b9d151a6bd5f17414af10e5dae9a79b
drednor <42916508+drednor@users.noreply.github.com>
2022-08-18 10:55:06 +0530

    Time: 230 ms (65.62%), Space: 15.5 MB (34.89%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -3,10 +3,15 @@ class Solution:
         # c = Counter(nums).most_common()
         # return c[0][0]
         memo = {}
+        temp = [None, float("-inf")]
         for num in nums:
             memo[num] = 1 + memo[num] if num in memo else 1
-        temp = [None,float("-inf")]
-        for key, val in memo.items():
+            val = memo[num]
             if val > temp[1]:
-                temp = [key, val]
+                temp = [num, val]
         return temp[0]
+        # temp = [None,float("-inf")]
+        # for key, val in memo.items():
+        #     if val > temp[1]:
+        #         temp = [key, val]
+        # return temp[0]
