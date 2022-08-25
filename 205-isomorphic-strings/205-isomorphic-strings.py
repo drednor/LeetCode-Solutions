a425e984c9f9ee3d6f884ac2717455fcc8b1a9fd
drednor <42916508+drednor@users.noreply.github.com>
2022-08-25 13:31:40 +0530

    Time: 54 ms (73.43%), Space: 14.2 MB (89.09%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,5 +1,62 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        memo = {}
+        memo2 = {}
+        for i in range(len(s)):
+            if s[i] in memo and memo[s[i]] != t[i]:
+                return False
+            if t[i] in memo2 and memo2[t[i]] != s[i]:
+                return False
+            memo[s[i]] = t[i]
+            memo2[t[i]] = s[i]
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         temp ={}
         temp2 = {}
         for i in range(len(s)):
