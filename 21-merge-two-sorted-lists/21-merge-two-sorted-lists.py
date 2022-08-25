df41c356fa8553bbcccc9af351b32d16f6c3cb33
drednor <42916508+drednor@users.noreply.github.com>
2022-08-25 15:17:31 +0530

    Time: 72 ms (21.06%), Space: 13.9 MB (79.86%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,20 +5,83 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = result = ListNode(0)
+        if list1 == None and list2 == None:
+            return None
+        dummy = ListNode(0,None)
+        head = dummy
         while list1 and list2:
-            if list1.val == list2.val:
-                result.next = list1
+            if list1.val <= list2.val:
+                head.next = list1
                 list1 = list1.next
-            elif list1.val > list2.val:
-                result.next = list2
-                list2 = list2.next
             else:
-                result.next = list1
-                list1 = list1.next
-            result = result.next
-        result.next = list1 or list2
+                head.next = list2
+                list2 = list2.next
+            head = head.next
+        if list1:
+            head.next = list1
+        else:
+            head.next = list2
         return dummy.next
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #         dummy = result = ListNode(0)
+#         while list1 and list2:
+#             if list1.val == list2.val:
+#                 result.next = list1
+#                 list1 = list1.next
+#             elif list1.val > list2.val:
+#                 result.next = list2
+#                 list2 = list2.next
+#             else:
+#                 result.next = list1
+#                 list1 = list1.next
+#             result = result.next
+#         result.next = list1 or list2
+#         return dummy.next
         
         
         
