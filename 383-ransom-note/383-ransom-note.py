fa862ac7d1b45a324579ae4d18b83d35de821ec3
drednor <42916508+drednor@users.noreply.github.com>
2022-08-25 11:04:58 +0530

    Time: 126 ms (24.56%), Space: 14.1 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,7 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        r = Counter(ransomNote)
-        m = Counter(magazine)
+        r = {}
+        m = {}
+        for char in ransomNote:
+            r[char] = 1 + r[char] if char in r else 1
+        for char in magazine:
+            m[char] = 1 + m[char] if char in m else 1
         for key, val in r.items():
             if key in m and r[key] <= m[key]:
                 continue
