f296ac0d9e06d359aa2eefea55be4abc2b60ae01
drednor <42916508+drednor@users.noreply.github.com>
2022-08-26 13:17:17 +0530

    Time: 62 ms (51.34%), Space: 13.9 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -2,15 +2,18 @@ class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         top = 1000000000
         i = 1
+        c = Counter(str(n))
         check = []
         while i <= top:
             temp = Counter(str(i))
-            check.append(temp)
+            if temp == c:
+                return True
             i *= 2
-        c = Counter(str(n))
-        if c in check:
-            return True
         return False
+        
+        # if c in check:
+        #     return True
+        # return False
         
         
         
