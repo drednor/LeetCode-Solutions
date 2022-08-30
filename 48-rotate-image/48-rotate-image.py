1057b102c0c806b948ccbc5b82e8ff0374fbda20
drednor <42916508+drednor@users.noreply.github.com>
2022-08-31 02:05:39 +0530

    Time: 35 ms (95.29%), Space: 13.9 MB (74.25%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,63 +3,29 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        l , r = 0, len(matrix)-1
+        l = 0
+        r = len(matrix)-1
         while l < r:
             for i in range(r-l):
-                top , bottom = l , r
+                top , bot = l ,r
                 topleft = matrix[top][l+i]
-                matrix[top][l+i] = matrix[bottom-i][l]
-                matrix[bottom-i][l] = matrix[bottom][r-i]
-                matrix[bottom][r-i] = matrix[top+i][r]
+                matrix[top][l+i] = matrix[bot-i][l]
+                matrix[bot-i][l] = matrix[bot][r-i]
+                matrix[bot][r-i] = matrix[top+i][r]
                 matrix[top+i][r] = topleft
             l+=1
             r-=1
+        
+
+        
+        # l , r = 0, len(matrix)-1
+        # while l < r:
+        #     for i in range(r-l):
+        #         top , bottom = l , r
+        #         topleft = matrix[top][l+i]
+        #         matrix[top][l+i] = matrix[bottom-i][l]
+        #         matrix[bottom-i][l] = matrix[bottom][r-i]
+        #         matrix[bottom][r-i] = matrix[top+i][r]
+        #         matrix[top+i][r] = topleft
+        #     l+=1
+        #     r-=1
