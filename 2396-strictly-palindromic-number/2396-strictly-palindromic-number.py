8a0393bd49479234b0c65acead6124f728c0d10f
drednor <42916508+drednor@users.noreply.github.com>
2022-09-04 03:13:13 +0530

    Time: 64 ms (15.38%), Space: 13.8 MB (69.23%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,3 +1,11 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        return False
+        for b in range(2,n-1):
+            temp = n
+            s = ""
+            while temp:
+                s += str(temp%b)
+                temp //= b
+            if s[::-1] != s:
+                return False
+        return True
