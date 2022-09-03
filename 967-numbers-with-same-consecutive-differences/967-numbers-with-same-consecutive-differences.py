4f8a8657b8a035b55d3a5c5351bcd6bc5391b3b1
drednor <42916508+drednor@users.noreply.github.com>
2022-09-04 00:22:36 +0530

    Time: 84 ms (20.75%), Space: 14.1 MB (73.81%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,22 +1,27 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        q = deque()
-        res = []
-        for i in range(10):
-            q.append([i])
+        temp = range(10)
+        for i in range(n-1):
+            temp = {x*10+y for x in temp for y in [x%10+k,x%10-k] if x and 0<=y<10}
+        return list(temp)
         
-        while q:
-            l = q.popleft()
-            if len(l) == n:
-                l = [str(d) for d in l]
-                num = int("".join(l))
-                if len(str(num)) == n: res.append(num)
-            else:
-                nx = l[-1] - k
-                if 0<= nx<10:
-                    q.append(l+[nx])
-                nx = l[-1] + k
-                if 0 <= nx <10:
-                    q.append(l+[nx])
-        return list(set(res))
+        #         q = deque()
+#         res = []
+#         for i in range(10):
+#             q.append([i])
+        
+#         while q:
+#             l = q.popleft()
+#             if len(l) == n:
+#                 l = [str(d) for d in l]
+#                 num = int("".join(l))
+#                 if len(str(num)) == n: res.append(num)
+#             else:
+#                 nx = l[-1] - k
+#                 if 0<= nx<10:
+#                     q.append(l+[nx])
+#                 nx = l[-1] + k
+#                 if 0 <= nx <10:
+#                     q.append(l+[nx])
+#         return list(set(res))
                 
