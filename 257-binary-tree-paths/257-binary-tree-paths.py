ca9aa448e5ac558f3ba5d0cba1d61f0a6e83edf8
drednor <42916508+drednor@users.noreply.github.com>
2022-09-14 14:44:50 +0530

    Time: 44 ms (71.79%), Space: 14 MB (29.52%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.py
+++ 257-binary-tree-paths/257-binary-tree-paths.py
@@ -6,15 +6,28 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        stack = [(root,"")]
+        # stack = [(root,"")]
+        # res = []
+        # while stack:
+        #     node , ls = stack.pop()
+        #     if not node.right  and not node.left:
+        #         res.append(ls + str(node.val))
+        #     if node.right:
+        #         stack.append((node.right, ls + str(node.val) + "->"))
+        #     if node.left:
+        #         stack.append((node.left, ls + str(node.val) + "->"))
+        # return res
         res = []
-        while stack:
-            node , ls = stack.pop()
-            if not node.right  and not node.left:
-                res.append(ls + str(node.val))
+        def dfs(node, temp):
+            if not node:
+                return 
+            if not node.right and not node.left:
+                res.append(temp + str(node.val))
             if node.right:
-                stack.append((node.right, ls + str(node.val) + "->"))
+                dfs(node.right, temp + str(node.val) + "->")
             if node.left:
-                stack.append((node.left, ls + str(node.val) + "->"))
+                dfs(node.left, temp + str(node.val) + "->")
+            return
+        dfs(root, "")
         return res
         
