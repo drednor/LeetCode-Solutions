122d812171fd9a16419931dff13c2086129e9626
drednor <42916508+drednor@users.noreply.github.com>
2022-09-24 05:17:25 +0530

    Time: 120 ms (6.41%), Space: 13.9 MB (69.05%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,16 +1,35 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        res = []
-        c = Counter(s)
-        visit = set()
-        temp = 0
+        last = {}
         for i in range(len(s)):
-            visit.add(s[i])
-            temp += 1
-            c[s[i]] -= 1
-            if c[s[i]] == 0:
-                visit.remove(s[i])
-            if len(visit) == 0:
-                res.append(temp)
-                temp = 0
+            last[s[i]] = i
+        size = 0
+        res = []
+        end = 0
+        print(last)
+        for i in range(len(s)):
+            size += 1
+            end = max(end, last[s[i]])
+            if i == end:
+                res.append(size)
+                size = 0
         return res
+            
+        
+        
+        # res = []
+        # c = Counter(s)
+        # visit = set()
+        # temp = 0
+        # for i in range(len(s)):
+        #     visit.add(s[i])
+        #     temp += 1
+        #     c[s[i]] -= 1
+        #     if c[s[i]] == 0:
+        #         visit.remove(s[i])
+        #     if len(visit) == 0:
+        #         res.append(temp)
+        #         temp = 0
+        # return res
+        
+        
