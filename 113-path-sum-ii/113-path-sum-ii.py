cb12c8ca8d90eb12e8a79de36739dadef065f65a
drednor <42916508+drednor@users.noreply.github.com>
2022-09-24 22:53:23 +0530

    Time: 83 ms (37.77%), Space: 15.2 MB (94.93%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -6,22 +6,42 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if root is None:
+            return []
         res = []
-        def dfs(node, temp, target):
-            if not node:
-                return
-            temp.append(node.val)
-            target -= node.val
-            if node.right == None and node.left == None:
-                if target == 0:
-                    res.append(temp.copy())
-            else:
-                dfs(node.left, temp,target)
-                dfs(node.right, temp, target)
-            temp.pop()
-            return 
-        dfs(root, [], targetSum)
+        temp = []
+        stack =[(root, targetSum - root.val, temp + [root.val])]
+        while stack:
+            cur, target, check = stack.pop()
+            temp.append(cur.val)
+            if not cur.left and not cur.right and target == 0:
+                    res.append(check)
+            if cur.right:
+                stack.append((cur.right, target-cur.right.val, check + [cur.right.val]))
+            if cur.left:
+                stack.append((cur.left, target-cur.left.val, check + [cur.left.val]))
         return res
+    
+                
+        
+        
+        
+        # res = []
+        # def dfs(node, temp, target):
+        #     if not node:
+        #         return
+        #     temp.append(node.val)
+        #     target -= node.val
+        #     if node.right == None and node.left == None:
+        #         if target == 0:
+        #             res.append(temp.copy())
+        #     else:
+        #         dfs(node.left, temp,target)
+        #         dfs(node.right, temp, target)
+        #     temp.pop()
+        #     return 
+        # dfs(root, [], targetSum)
+        # return res
             
         
         
