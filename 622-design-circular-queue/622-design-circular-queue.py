25611bb31015f211f531e624619526bbaaff27cd
drednor <42916508+drednor@users.noreply.github.com>
2022-09-25 20:22:39 +0530

    Time: 162 ms (10.85%), Space: 14.9 MB (6.94%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.py
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -1,46 +1,45 @@
-class ListNode:
-    def __init__(self, val, nxt, prev):
-        self.val , self.next, self.prev = val, nxt, prev
+class Node:
+    def __init__(self, value, nxt, prev):
+        self.val = value
+        self.next = nxt
+        self.prev = prev
 
 class MyCircularQueue:
 
     def __init__(self, k: int):
         self.size = k
-        self.left = ListNode(0, None, None)
-        self.right = ListNode(0,None, self.left)
+        self.left = Node(0,None,None)
+        self.right = Node(0,None,self.left)
         self.left.next = self.right
-        
+            
 
     def enQueue(self, value: int) -> bool:
         if self.isFull():return False
-        cur = ListNode(value, self.right, self.right.prev)
+        cur = Node(value, self.right, self.right.prev)
         self.right.prev.next = cur
         self.right.prev = cur
         self.size -= 1
         return True
-        
 
     def deQueue(self) -> bool:
         if self.isEmpty():return False
         cur = self.left.next
-        self.left.next = self.left.next.next
-        self.left.next.prev = self.left
+        self.left.next = cur.next
+        cur.next.prev = self.left
         self.size += 1
         return True
-        
 
     def Front(self) -> int:
         if self.isEmpty():return -1
         return self.left.next.val
+        
 
     def Rear(self) -> int:
-        if self.isEmpty(): return -1
+        if self.isEmpty():return -1
         return self.right.prev.val
-        
 
     def isEmpty(self) -> bool:
         return self.left.next == self.right
-        
 
     def isFull(self) -> bool:
         return self.size == 0
