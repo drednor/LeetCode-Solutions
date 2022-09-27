edd0745694e6a3c56ba610f950322bc4838c37b3
drednor <42916508+drednor@users.noreply.github.com>
2022-09-27 21:42:33 +0530

    Time: 782 ms (34.12%), Space: 22.3 MB (27.57%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.py
+++ 838-push-dominoes/838-push-dominoes.py
@@ -2,21 +2,21 @@ class Solution:
     def pushDominoes(self, dominoes: str) -> str:
         dom = list(dominoes)
         q = deque()
-        for i , char in enumerate(dominoes):
+        for i, char in enumerate(dom):
             if char != ".":
-                q.append((i,char))
+                q.append((i, char))
         
         while q:
             i, char = q.popleft()
-            
+            #print(q)
             if char == "L":
                 if i > 0 and dom[i-1] == ".":
                     q.append((i-1,"L"))
                     dom[i-1] = "L"
-            
-            elif char == "R":
-                if i + 1 < len(dom) and dom[i+1] == ".":
-                    if i+2 <len(dom) and dom[i+2] == "L":
+                
+            if char == "R":
+                if i + 1< len(dom) and dom[i+1] == ".":
+                    if i+2 < len(dom) and dom[i+2] == "L":
                         q.popleft()
                     else:
                         q.append((i+1,"R"))
