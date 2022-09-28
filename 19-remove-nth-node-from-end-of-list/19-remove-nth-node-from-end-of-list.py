b8b0c01a72a41323cb6f560f8180a6c7f360300f
drednor <42916508+drednor@users.noreply.github.com>
2022-09-28 05:43:38 +0530

    Time: 77 ms (6.24%), Space: 13.9 MB (70.33%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,78 +5,22 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        '''
-                 4          
-        1->2->3->4->5->6->7
-              s---->
-        f           f      f 
-        '''         
-        slow = fast = head
-        for i in range(n):
-            fast = fast.next
-        if not fast:
+        if head.next is None and n == 1:
+            return None
+        cur = head
+        total = 0
+        while cur:
+            total += 1
+            cur = cur.next
+        cur = head
+        print(total)
+        if total == n:
             return head.next
-        while fast.next:
-            slow = slow.next
-            fast = fast.next
-        slow.next = slow.next.next
+        n = total - n
+        i = 0
+        while i < n-1:
+            cur = cur.next
+            i+=1
+        cur.next = cur.next.next
         return head
-              
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        
-        
-        # fast = slow = head
-        # for _ in range(n):
-        #     fast = fast.next
-        # if not fast:
-        #     return head.next
-        # while fast.next:
-        #     fast = fast.next
-        #     slow = slow.next
-        # slow.next = slow.next.next
-        # return head
-        
-        # a = head
-        # lenA = 0
-        # while a:
-        #     lenA += 1
-        #     a = a.next
-        # x = lenA-n
-        # a = head
-        # if x==0 and head.next:
-        #     head = head.next
-        #     return head
-        # elif x==0 and head.next is None:
-        #     head = None
-        #     return head
-        # else:
-        #     for i in range(x-1):
-        #         a = a.next
-        #     a.next = a.next.next
-        # return head
-            
         
