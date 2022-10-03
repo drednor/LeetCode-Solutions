b71041febde296f6bf9aceacd0cb74b135af81c4
drednor <42916508+drednor@users.noreply.github.com>
2022-10-03 06:47:32 +0530

    Time: 2384 ms (37.98%), Space: 24.9 MB (74.83%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,32 +1,32 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        res = prev = 0
-        for i in range(1, len(colors)):
-            if colors[prev] != colors[i]:
-                prev = i
-            else:
-                res += min(neededTime[i], neededTime[prev])
-                if neededTime[prev]< neededTime[i]:
-                    prev = i
-        return res
-        
-        
-        # n = len(colors)
-        # prev = (None, None)
-        # i = 0 
-        # res = 0
-        # while i < n:
-        #     pc, pt = prev
-        #     if colors[i] == pc:
-        #         if neededTime[i] >= pt:
-        #             res += pt
-        #         else:
-        #             res += neededTime[i]
-        #             i+=1
-        #             continue
-        #     prev = (colors[i],neededTime[i])
-        #     i+=1
+        # res = prev = 0
+        # for i in range(1, len(colors)):
+        #     if colors[prev] != colors[i]:
+        #         prev = i
+        #     else:
+        #         res += min(neededTime[i], neededTime[prev])
+        #         if neededTime[prev]< neededTime[i]:
+        #             prev = i
         # return res
+        
+        
+        n = len(colors)
+        prev = (None, None)
+        i = 0 
+        res = 0
+        while i < n:
+            pc, pt = prev
+            if colors[i] == pc:
+                if neededTime[i] >= pt:
+                    res += pt
+                else:
+                    res += neededTime[i]
+                    i+=1
+                    continue
+            prev = (colors[i],neededTime[i])
+            i+=1
+        return res
                     
                 
                     
