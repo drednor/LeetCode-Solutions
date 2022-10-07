0607ad11470f0c836a06ad58fa9a15f53eb5157e
drednor <42916508+drednor@users.noreply.github.com>
2022-10-07 17:03:22 +0530

    Time: 4161 ms (9.54%), Space: 14.4 MB (77.23%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.py
+++ 732-my-calendar-iii/732-my-calendar-iii.py
@@ -1,17 +1,19 @@
 class MyCalendarThree:
 
     def __init__(self):
-        self.check = defaultdict(int)
-        
+        self.bookings = defaultdict(int)
+
     def book(self, start: int, end: int) -> int:
-        self.check[start] += 1
-        self.check[end] -= 1
+        self.bookings[start] += 1
+        self.bookings[end] -= 1
+        
         res = 0
         inter = 0
-        for key in sorted(self.check):
-            inter += self.check[key]
+        for key in sorted(self.bookings):
+            inter += self.bookings[key]
             res = max(res, inter)
         return res
+
 
 # Your MyCalendarThree object will be instantiated and called as such:
 # obj = MyCalendarThree()
