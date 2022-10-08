f44ef4c3300b682fd4efe11688a7cafb217bae2e
drednor <42916508+drednor@users.noreply.github.com>
2022-10-08 09:02:09 +0530

    Time: 4319 ms (87.72%), Space: 14.1 MB (53.95%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -22,3 +22,25 @@ class Solution:
                     r -= 1
         return res
                                 
+        
+        # my Solution - Correct solution but does not work with python language on leetcode
+        # if len(nums) == 3:
+        #     return sum(nums)
+        # nums.sort()
+        # res = [[None,None,None],float("inf")]
+        # for i , n in enumerate(nums):
+        #     l = i +1
+        #     r = len(nums)-1
+        #     while l < r:
+        #         temp = res[1]
+        #         threesum = n + nums[l] + nums[r]
+        #         res[1] = min(res[1],abs(target-(threesum)))
+        #         if temp != res[1]:
+        #             res[0] = [i,l,r]
+        #         if target - threesum > 0:
+        #             l += 1
+        #         elif target - threesum < 0:
+        #             r -= 1
+        #         else:
+        #             return target
+        # return nums[res[0][0]] + nums[res[0][1]] + nums[res[0][2]]
