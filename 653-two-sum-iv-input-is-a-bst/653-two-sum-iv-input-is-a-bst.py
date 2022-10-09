f6e3980fc49bf31e41056673c18250a13048b68e
drednor <42916508+drednor@users.noreply.github.com>
2022-10-09 06:54:26 +0530

    Time: 187 ms (29.70%), Space: 18.5 MB (36.45%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -7,20 +7,34 @@
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
         memo = {}
-        memo[k-root.val] = 1
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            if node.right:
-                if node.right.val in memo:
-                    return True
-                else:
-                    stack.append(node.right)
-                    memo[k-node.right.val] = 1
-            if node.left:
-                if node.left.val in memo:
-                    return True
-                else:
-                    stack.append(node.left)
-                    memo[k-node.left.val] = 1
-        return False
+        def recur(node):
+            if not node:
+                return False
+            if node.val in memo:
+                return True
+            memo[k-node.val] = 1
+            return recur(node.right) or recur(node.left)
+        return recur(root)
+        
+        
+        
+        
+        
+        # memo = {}
+        # memo[k-root.val] = 1
+        # stack = [root]
+        # while stack:
+        #     node = stack.pop()
+        #     if node.right:
+        #         if node.right.val in memo:
+        #             return True
+        #         else:
+        #             stack.append(node.right)
+        #             memo[k-node.right.val] = 1
+        #     if node.left:
+        #         if node.left.val in memo:
+        #             return True
+        #         else:
+        #             stack.append(node.left)
+        #             memo[k-node.left.val] = 1
+        # return False
