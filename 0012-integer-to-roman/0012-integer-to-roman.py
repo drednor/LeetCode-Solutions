d89abb4bded30454c0a4a5ed15a2a781ae719dc4
drednor <42916508+drednor@users.noreply.github.com>
2022-10-20 06:13:21 +0530

    Time: 69 ms (77.32%), Space: 13.8 MB (80.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,67 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        memo = {
+            "IIII":"IV",
+            "VIIII":"IX",
+            "XXXX":"XL",
+            "LXXXX":"XC",
+            "CCCC":"CD",
+            "DCCCC":"CM"
+        }
+        res = ""
+        while num:
+            if num<5:
+                if num == 4:
+                    res += "IV"
+                    num -= num
+                else:
+                    res += num*"I"
+                    num -= num
+            elif 5<=num<10:
+                if num >= 9:
+                    res += "IX"
+                    num -= 9
+                else:
+                    res += "V"
+                    num -= 5
+            elif 10<=num<50:
+                if num >= 40:
+                    res+= "XL"
+                    num -= 40
+                else:
+                    res += "X"
+                    num -= 10
+            elif 50<=num<100:
+                if num >= 90:
+                    res+= "XC"
+                    num -= 90
+                else:
+                    res += "L"
+                    num -= 50
+            elif 100<=num<500:
+                if num >= 400:
+                    res+= "CD"
+                    num -= 400
+                else:
+                    res += "C"
+                    num -= 100
+            elif 500<=num<1000:
+                if num >= 900:
+                    res+= "CM"
+                    num -= 900
+                else:
+                    res += "D"
+                    num -= 500
+            elif num>=1000:
+                res += "M"
+                num -= 1000
+                
+        # for 
+        # print(res)
+        # res = res.replace("IIII","IV").replace("VIIII","IX")
+        # print(res)
+        # res = res.replace("XXXX","XL").replace("LXXXX","XC")
+        # print(res)
+        # res = res.replace("CCCC","CD").replace("DCCCC","CM")
+        # print(res)
+        return res
