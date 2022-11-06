cef89eee2a650549016c310bfe2026d5f8808b71
drednor <42916508+drednor@users.noreply.github.com>
2022-11-06 14:28:45 +0530

    Time: 1261 ms (88.85%), Space: 18.7 MB (8.99%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,15 +1,16 @@
 class TrieNode():
     def __init__(self):
-        self.children ={}
-        self.endOfWord = False
+        self.children = {}
+        self.endofword = False
+    
     
     def addword(self, word):
         cur = self
         for char in word:
             if char not in cur.children:
                 cur.children[char] = TrieNode()
-            cur = cur.children[char] 
-        cur.endOfWord = True
+            cur = cur.children[char]
+        cur.endofword = True
     
     def pruneWord(self, word) -> None:
         cur: TrieNode = self
@@ -24,35 +25,38 @@ class TrieNode():
                 del parentNode.children[childKey]
             else:
                 return
+    
+    
+        
             
-            
+
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         root = TrieNode()
         for word in words:
             root.addword(word)
+            
         rows, cols = len(board), len(board[0])
-        visited, result = set(), set()
-        
+        visited, res = set(), set()
         def dfs(r, c, node, word):
-            if (r<0 or c<0 or r==rows or c == cols or (r,c) in visited or board[r][c] not in node.children):
-                return
+            if r < 0 or c < 0 or r == rows or c==cols or (r,c) in visited or board[r][c] not in node.children:
+                return 
             visited.add((r,c))
             node = node.children[board[r][c]]
             word += board[r][c]
-            if node.endOfWord:
-                result.add(word)
-                node.isWord = False
+            if node.endofword:
+                res.add(word)
+                node.endofword = False
                 root.pruneWord(word)
-            dfs(r+1, c, node, word)
-            dfs(r, c+1, node, word)
-            dfs(r-1, c, node, word)
-            dfs(r, c-1, node, word)
+            
+            dfs(r+1,c,node,word)
+            dfs(r,c+1,node,word)
+            dfs(r-1,c,node,word)
+            dfs(r,c-1,node,word)
             visited.remove((r,c))
             
         for r in range(rows):
             for c in range(cols):
                 dfs(r, c, root, "")
+        return list(res)
         
-        return list(result)
-            
