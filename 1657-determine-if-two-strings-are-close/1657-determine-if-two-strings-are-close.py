86bccad8740fcfe9f6520f015c9e08b4699afb7e
drednor <42916508+drednor@users.noreply.github.com>
2022-12-02 06:45:25 +0530

    Time: 111 ms (99.60%), Space: 15.5 MB (16.80%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -8,13 +8,6 @@ class Solution:
             return False
         c = Counter(word1)
         d = Counter(word2)
-        i = 0
-        for k, v in c.items():
-            if k in d:
-                i+=1
-        if i <= 1:
-            return False
-        idx = set()
         x = c.values()
         y = d.values()
         temp = Counter(x)
